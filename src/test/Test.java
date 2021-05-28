54f304d3c0c836856bcf3e2335bde7ab03e68f8f
Carlos Plaza <c.plazaortega@gmail.com>
2021-05-28 20:45:27 +0200

    Test resta

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,13 +6,16 @@ import utils.Operaciones;
 
 public class Test {
 
-	@SuppressWarnings("deprecation")
 	@org.junit.Test
 	public void test() {
 		Operaciones op = new Operaciones();	
-		double resultSuma = op.suma(2, 2);	
-		System.out.println(resultSuma);
-		assertEquals("Suma 2+2=4", 4.0, resultSuma, 1e-5);		
+		
+		double resultSuma = op.suma(2, 2);			
+		assertEquals("Suma 2+2=4", 4.0, resultSuma, 1e-5);
+		
+		double resultResta = op.resta(4, 3);
+		assertEquals("Resta 4-3", 1.0, resultResta, 1e-5);
+		
 	}
 	
 	
