3f324df243c402266e12d88d41a6897d05788a71
Carlos Plaza <c.plazaortega@gmail.com>
2021-05-28 20:51:53 +0200

    Refactor and multiplication test

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,23 +5,27 @@ import static org.junit.Assert.*;
 import utils.Operaciones;
 
 public class Test {
+	Operaciones op = new Operaciones();	
 
 	@org.junit.Test
-	public void test() {
-		Operaciones op = new Operaciones();	
-		
+	public void test() {		 			
 		double resultSuma = op.suma(2, 2);			
-		assertEquals("Suma 2+2=4", 4.0, resultSuma, 1e-5);
-			
+		assertEquals("Suma 2+2=4", 4.0, resultSuma, 1e-5);			
 	}
 	
 	
 	@org.junit.Test
 	public void testResta() {
-		Operaciones op = new Operaciones();	
-		
-		double resultResta = op.resta(4, 3);
-		assertEquals("Resta 4-3", 1.0, resultResta, 1e-5);
+		double result = op.resta(4, 3);
+		assertEquals("Resta 4-3", 1.0, result, 1e-5);
+			
+	}
+	
+	
+	@org.junit.Test
+	public void testMulti() {		
+		double result = op.multiplicacion(4, 3);
+		assertEquals("Multiplicacion 4*3=12", 12.0, result, 1e-5);
 			
 	}
 	
