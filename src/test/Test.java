581f04456ac7b4314b011a0264f5887be2c97cd9
Carlos Plaza <c.plazaortega@gmail.com>
2021-05-28 20:47:21 +0200

    Nuevo metodo para test

--- src/test/Test.java
+++ src/test/Test.java
@@ -12,11 +12,20 @@ public class Test {
 		
 		double resultSuma = op.suma(2, 2);			
 		assertEquals("Suma 2+2=4", 4.0, resultSuma, 1e-5);
+			
+	}
+	
+	
+	@org.junit.Test
+	public void testResta() {
+		Operaciones op = new Operaciones();	
 		
 		double resultResta = op.resta(4, 3);
 		assertEquals("Resta 4-3", 1.0, resultResta, 1e-5);
-		
+			
 	}
+	
+	
 	
 	
 	
